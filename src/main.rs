d23266dbb899a1f1ec263a5466a7c0bf0959eaac
amudhan23 <amudhanmuthaiah@yahoo.in>
2026-08-06 20:22:46 -0400

    db individual task

--- src/main.rs
+++ src/main.rs
@@ -15,11 +15,27 @@ async fn main() {
 }
 
 async fn process(socket: TcpStream) {
-    let mut connection = Connection::new(socket);
-    if let Some(frame) = connection.read_frame().await.unwrap() {
-        println!("GOT: {:?}", frame);
+    use mini_redis::Command::{self, Get, Set};
+    use std::collections::HashMap;
+    let mut db = HashMap::new();
 
-        let response = Frame::Error("unimplemented".to_string());
+    let mut connection = Connection::new(socket);
+
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        let response = match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                Frame::Simple("OK".to_string())
+            }
+            Get(cmd) => {
+                if let Some(value) = db.get(cmd.key()) {
+                    Frame::Bulk(value.clone().into())
+                } else {
+                    Frame::Null
+                }
+            }
+            cmd => panic!("unimplemented {:?}", cmd),
+        };
         connection.write_frame(&response).await.unwrap();
     }
 }
